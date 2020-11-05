c2fb9ce34270bd66a26c7f228f2d680df05232ab
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-11-05 22:10:18 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -38,7 +38,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_input=gets.strip
     if (1...songs.length).include?(user_input.to_i)
-      puts "Playing #{songs[user_input.to_i]}"
+      puts "Playing #{songs[user_input.to_i - 1]}"
     elsif songs.include?(user_input)
       puts "Playing #{songs}"
     else
